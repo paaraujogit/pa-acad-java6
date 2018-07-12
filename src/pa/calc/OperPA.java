0940f93347f64be96f6f04d5775346f29e79d43d
rform <rform@UED-PC-326.EDIGITAL.EDU.LEARN.LOCAL>
2018-07-12 20:55:23 +0100

    Alteração OperPA Comentário

--- src/pa/calc/OperPA.java
+++ src/pa/calc/OperPA.java
@@ -4,6 +4,6 @@ public class OperPA implements ItfOper {
 
 		@Override
 		public double oper(double op1, double op2) {
-			return Math.sqrt(op1*op1+op2*op2);
+			return Math.sqrt(op1*op1+op2*op2); // Teorema pitágoras
 		}
 }
