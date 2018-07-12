26daf798678e53935e5329491e898b0ea28ddfea
Formação <rform@EDIGITAL.EDU.LEARN.LOCAL>
2018-07-12 21:03:23 +0100

    adicinado file OperSi

--- src/pa/calc/Calculadora.java
+++ src/pa/calc/Calculadora.java
@@ -4,16 +4,16 @@ public class Calculadora {
 
 	public static void main(String[] args) {
 		
-		// OperEx Example
+/*		// OperEx Example
 		OperEx opex=new OperEx();
-		double resEx = opex.oper(1, 2);
+		double resEx = opex.oper(1, 3);
 		System.out.println(resEx);
-		
+		*/
 		// ToDo - All Students Must Implement one Operation
-		// - Oper0X - Name
-	    // Oper0X ope0X=new Oper0X();
-	    // double res0X = ope0X.oper(X, Y);
-	    // System.out.println(res0X);
+		// - Oper0X - Simone
+	     OperSi opex=new OperSi();
+	     double res0X = opex.oper(5, 4);
+	     System.out.println(res0X);
 	}
 
 }
