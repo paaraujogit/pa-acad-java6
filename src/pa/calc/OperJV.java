2a2f879be91211262f1c85c2b0c5c66dbf5daee2
Jorge Vargas <jvargas.net@gmail.com>
2018-07-14 12:17:53 +0100

    Oper JV

---
+++ src/pa/calc/OperJV.java
@@ -0,0 +1,9 @@
+package pa.calc;
+
+public class OperJV implements ItfOper {
+
+		@Override
+		public double oper(double op1, double op2) {
+			return Math.sqrt(op1*op1+op2*op2); // Teorema pitágoras
+		}
+}
