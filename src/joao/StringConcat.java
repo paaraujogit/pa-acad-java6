202134c8f671c0dd34bd8457f9c8b813fea43b34
João Rosa <joao.f.a.rosa@gmail.com>
2018-07-14 15:41:07 +0100

    Package João (String Concat).

---
+++ src/joao/StringConcat.java
@@ -0,0 +1,7 @@
+package joao;
+
+public class StringConcat {
+		public String TwoStrings(String a, String b) {
+			return a + b;
+		}
+}
---
+++ src/joao/StringConcatExe.java
@@ -0,0 +1,10 @@
+package joao;
+
+public class StringConcatExe {
+
+	public static void main(String[] args) {
+		StringConcat phrase = new StringConcat();
+		System.out.println("A frase é: " + phrase.TwoStrings("Hello ", "world"));
+	}
+
+}
